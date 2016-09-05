39252593ea109d6e2fa690eb504f9760b47036ad
Natasja Pilemand <Natasja Pilemand@NATASJA>
2016-09-05 16:05:41 +0200

    My solution to 00_Hello.

---
+++ 00_Hello/Functions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_Hello
+{
+    class Functions
+    {
+        internal static string Hello()
+        {
+            return "hello";
+        }
+
+        internal static object Hello(string v)
+        {
+            return "hello, " + v;
+        }
+    }
+}
